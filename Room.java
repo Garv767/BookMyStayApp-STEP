507a02d27e9302bfa651ce95c3c25c4cfcc4177a
Garv767 <garvrahut@gmail.com>
2026-03-10 22:45:55 +0530

    feat: UC4 Room Search & Availability Check

--- Room.java
+++ Room.java
@@ -1,7 +1,6 @@
 /**
  * ABSTRACT CLASS - Room
  * Represents a generic hotel room with intrinsic attributes.
- * @version 2.1
  */
 public abstract class Room {
     protected int numberOfBeds;
@@ -19,4 +18,16 @@ public abstract class Room {
         System.out.println("Size: " + squareFeet + " sqft");
         System.out.println("Price per night: " + pricePerNight);
     }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() { super(1, 250, 1500.0); }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() { super(2, 400, 2500.0); }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() { super(3, 750, 5000.0); }
 }
